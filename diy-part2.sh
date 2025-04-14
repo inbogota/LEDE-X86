4a7e97ac8ef7ae55c7c29c34f1ec5361fda189db
Professor <61362125+inbogota@users.noreply.github.com>
2025-04-14 08:28:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,6 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 rm -rf package/lean/luci-theme-argon
